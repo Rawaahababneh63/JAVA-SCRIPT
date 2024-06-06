f70add82e22b853b9ffb5b38c2181c153b07c8ef
Rawaahababneh63 <rawaahabedelrahamn@gmail.com>
2024-06-06 03:37:28 +0300

    JAVAA

---
+++ task.js
@@ -0,0 +1,92 @@
+var newtext = document.getElementById('text');
+
+newtext.addEventListener("mouseout", function() {
+    newtext.textContent = "Can I help you"
+  })
+
+newtext.addEventListener("mouseover", function() {
+    newtext.textContent = "Hello World"})
+
+
+
+   
+
+
+function image(){
+   var y=document.getElementById("change").value;
+if(y=="jordan"){
+     document.getElementById ("im").src="/image/Jordan.jpeg";
+    
+
+}
+else(     document.getElementById ("im").src="/image/palastine.jpeg");
+
+
+    }
+
+
+//   //////////////////////////// 
+const fonttype=document.getElementById("font");
+fonttype.addEventListener("click",changefont);
+
+
+
+function changefont(){
+    let x=document.getElementById("par");
+    if(fonttype.value=="Times New Roman")
+        {
+            x.style.fontFamily="Times New Roman";
+        }
+else( x.style.fontFamily="Fantasy")
+}
+
+
+let size=document.getElementById("font-size");
+size.addEventListener("click",changesize);
+
+function changesize(){
+let s=document.getElementById("par");
+if(size.value== "10pt"){
+s.style.fontSize="10pt"
+}
+else(
+    s.style.fontSize="15pt"
+)
+}
+
+
+
+let I=document.getElementById("Italic");
+I.addEventListener("click" ,checked);
+
+function checked(){
+  var a=document.getElementById("par");
+  if(I.checked) { a.style.fontStyle="Italic";
+}
+else{a.style.fontStyle="normal";}
+}
+
+
+let b=document.getElementById("Bold");
+b.addEventListener("click" ,checked1);
+
+function checked1(){
+  var a=document.getElementById("par");
+  if(b.checked) { a.style.fontWeight="Bold";
+}
+else{a.style.fontWeight="normal";}
+}
+
+
+
+
+
+let U=document.getElementById("Underline");
+U.addEventListener("click" ,checked2);
+
+function checked2(){
+  var a=document.getElementById("par");
+  if(U.checked) { a.style.textDecorationLine="Underline";
+}
+else{a.style.textDecorationLine="none";}
+}
